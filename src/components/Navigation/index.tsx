2c377de35eec54c98345fb67fe30a04fd64e8c4c
Ray <huynh.r.k@gmail.com>
2021-11-02 22:52:15 -0700

    Add some styling to navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,15 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Flex, HStack } from "@chakra-ui/react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import { Flex, HStack, Link, Text } from "@chakra-ui/react";
 
 const Navigation = () => {
+  const { pathname } = useLocation();
+
+  const path = pathname.slice(1);
+
   return (
     <Flex justify="flex-start" maxW="100%" w="100%">
-      <HStack spacing="6px">
-        <Link to="/">Home</Link>
-        <Link to="/collections">Collections</Link>
-        <Link to="/owned">Owned</Link>
-        <Link to="/watchList">Watch List</Link>
+      <HStack spacing="12px">
+        <RouterLink to="/">
+          <Text color={path === "" ? "green.300" : "grey.900"}>Home</Text>
+        </RouterLink>
+        <RouterLink to="/collections">
+          <Text color={path.includes("collections") ? "green.300" : "grey.900"}>
+            Collections
+          </Text>
+        </RouterLink>
+        <RouterLink to="/owned">
+          <Text color={path.includes("owned") ? "green.300" : "grey.900"}>
+            Owned
+          </Text>
+        </RouterLink>
+        <RouterLink to="/watchList">
+          <Text color={path.includes("watchList") ? "green.300" : "grey.900"}>
+            Watch List
+          </Text>
+        </RouterLink>
       </HStack>
     </Flex>
   );
