5de887e1dc3287e6a8083143cde02dc265282678
Ray <huynh.r.k@gmail.com>
2021-11-02 22:54:48 -0700

    Remove home link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Flex, Box, Button, HStack, Text, Input } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
 
 const Header = () => {
+  const history = useHistory();
+
   return (
     <Flex justify="space-between" w="100%" mt="20px">
       <Box>
         <HStack spacing={4}>
           <Box>
-            <Text fontSize="xl">NiftyWatcher</Text>
+            <Text
+              fontSize="xl"
+              onClick={() => history.push("/")}
+              cursor="pointer"
+            >
+              NiftyWatcher
+            </Text>
           </Box>
           <Box>
             <Input placeholder="Search for collection" maxW="800px" w="100%" />
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link as RouterLink, useLocation } from "react-router-dom";
-import { Flex, HStack, Link, Text } from "@chakra-ui/react";
+import { Flex, HStack, Text } from "@chakra-ui/react";
 
 const Navigation = () => {
   const { pathname } = useLocation();
@@ -10,9 +10,6 @@ const Navigation = () => {
   return (
     <Flex justify="flex-start" maxW="100%" w="100%">
       <HStack spacing="12px">
-        <RouterLink to="/">
-          <Text color={path === "" ? "green.300" : "grey.900"}>Home</Text>
-        </RouterLink>
         <RouterLink to="/collections">
           <Text color={path.includes("collections") ? "green.300" : "grey.900"}>
             Collections
