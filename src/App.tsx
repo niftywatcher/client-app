7da41af77db0a401b6b1aef5a1f8b9a791059eec
Ray <huynh.r.k@gmail.com>
2021-11-02 21:19:33 -0700

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import Collections from "./routes/collections/index";
+import Home from "./routes";
 
 const theme = extendTheme({
   styles: {
@@ -14,7 +17,24 @@ const theme = extendTheme({
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <div>hello world</div>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/collections">Collections</Link>
+          </li>
+        </ul>
+        <Switch>
+          <Route path="/collections">
+            <Collections />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </ChakraProvider>
   );
 }
