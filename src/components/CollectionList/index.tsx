50639869de18841751c130de9080116bb3078c75
Ray <huynh.r.k@gmail.com>
2021-11-11 16:03:17 -0800

    Fix seed data

--- src/components/CollectionList/index.tsx
+++ src/components/CollectionList/index.tsx
@@ -5,8 +5,20 @@ import Card from "./Card";
 
 interface Collection {
   id: string;
-  name: string;
+  floor: number;
+  address: string;
+  description: string | null;
+  discordUrl: string | null;
+  externalUrl: string | null;
   imageUrl: string;
+  instagramUsername: string | null;
+  name: string;
+  symbol: string | null;
+  twitterUsername: string | null;
+  deltaStats: {
+    floor: number;
+  };
+  floorData: number[];
 }
 
 const CollectionList = () => {
