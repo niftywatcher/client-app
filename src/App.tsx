36dc7d3255ad885b22412f9c088d41c7230b42b3
Ray <huynh.r.k@gmail.com>
2021-11-17 08:00:07 -0800

    Add wallet connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { chakra, ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import { Web3ReactProvider } from "@web3-react/core";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 import SplitView from "./components/SplitView";
@@ -10,6 +11,11 @@ import dimensions from "./Shared/utils/dimensions";
 import collections from "./collections.json";
 import Collection from "./Shared/Interfaces/collection";
 import WatchList from "./Shared/Interfaces/WatchList";
+import Web3 from "web3";
+
+function getLibrary(provider: any, connector: any) {
+  return new Web3(provider); // this will vary according to whether you use e.g. ethers or web3.js
+}
 
 function App() {
   const collectionData: Collection[] = collections.slice(0, 25);
@@ -33,34 +39,36 @@ function App() {
   );
 
   return (
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <Header />
-        <chakra.main
-          height={`calc(100vh - ${dimensions.headerHeight}px)`}
-          overflow="hidden"
-        >
-          <SplitView
-            left={
-              <Navigation
-                activeWatchList={activeWatchList}
-                setActiveWatchList={setActiveWatchList}
-                watchLists={watchLists}
-                setWatchLists={setWatchLists}
-              />
-            }
-            right={
-              <CollectionList
-                collections={filteredCollections}
-                watchLists={watchLists}
-                setWatchLists={setWatchLists}
-              />
-            }
-            align="flex-start"
-          />
-        </chakra.main>
-      </ChakraProvider>
-    </BrowserRouter>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <Header />
+          <chakra.main
+            height={`calc(100vh - ${dimensions.headerHeight}px)`}
+            overflow="hidden"
+          >
+            <SplitView
+              left={
+                <Navigation
+                  activeWatchList={activeWatchList}
+                  setActiveWatchList={setActiveWatchList}
+                  watchLists={watchLists}
+                  setWatchLists={setWatchLists}
+                />
+              }
+              right={
+                <CollectionList
+                  collections={filteredCollections}
+                  watchLists={watchLists}
+                  setWatchLists={setWatchLists}
+                />
+              }
+              align="flex-start"
+            />
+          </chakra.main>
+        </ChakraProvider>
+      </BrowserRouter>
+    </Web3ReactProvider>
   );
 }
 
