51b1cff6e4d8baff2f0c798cdd5292d7c696c937
Ray <huynh.r.k@gmail.com>
2022-01-06 09:46:00 -0800

    Get signature

--- src/components/MetaMaskProvider/index.tsx
+++ src/components/MetaMaskProvider/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useWeb3React } from "@web3-react/core";
 import { useToast } from "@chakra-ui/react";
 import { useMutation } from "react-query";
@@ -11,8 +11,9 @@ import { getCookie } from "../../Shared/utils";
  * TODO: need to checkout uniswap way of handling this provider: https://github.com/Uniswap/interface/blob/13d7d2c99235aacd199641a9bfcae8aa6f7d94ae/src/components/Web3ReactManager/index.tsx#L22
  */
 
-interface GenerateNonceVariables {
-  address: string | null | undefined;
+const API_URL = process.env.REACT_APP_API || "";
+interface GenerateNonceVariables<T> {
+  address: T;
 }
 
 interface NonceReturn {
@@ -21,20 +22,45 @@ interface NonceReturn {
   };
 }
 
-const getNonce = async (
-  variables: GenerateNonceVariables
+const getNonceApi = async (
+  variables: GenerateNonceVariables<string>
 ): Promise<NonceReturn> => {
   const response = await request(
-    "http://localhost:8000/graphql",
+    API_URL,
     gql`
-      mutation auth($address: String!) {
+      mutation generateNonce($address: String!) {
         GenerateNonce(address: $address)
       }
     `,
     variables
   );
 
-  console.log({ response });
+  return response;
+};
+
+interface VerifySigVariables<T> {
+  address: T;
+  signature: String;
+}
+
+interface VerifySigReturn {
+  data: {
+    VerifySignature: boolean;
+  };
+}
+
+const verifySigApi = async (
+  variables: VerifySigVariables<string>
+): Promise<VerifySigReturn> => {
+  const response = await request(
+    API_URL,
+    gql`
+      mutation verifySignature($address: String!, $signature: String!) {
+        VerifySignature(address: $address, signature: $signature)
+      }
+    `,
+    variables
+  );
 
   return response;
 };
@@ -75,38 +101,40 @@ function MetamaskProvider({
       });
   }, [activateNetwork, networkActive, networkError, disconnect]);
 
-  const signMessage = async () => {
-    const nonce = "iPledgeToApe";
+  const { mutateAsync: mutateNonce } = useMutation(getNonceApi);
+  const { mutateAsync: mutateVerify } = useMutation(verifySigApi);
 
-    const signer = library.getSigner();
-    const sig = await signer.signMessage(nonce);
+  const getNonce = useCallback(async () => {
+    try {
+      if (typeof account === "string") {
+        const response = await mutateNonce({ address: account });
+        const nonce = response.data.GenerateNonce;
 
-    return sig;
-  };
+        return nonce;
+      }
+    } catch (err) {
+      console.log({ getNonceError: err });
+      return "8f13d9a7103441f37f8e7960de0c1469f2ddab11178a9a596967e91e51522c14";
+    }
+  }, [account, mutateNonce]);
 
-  const mutation = useMutation(getNonce, {
-    onError: async (err) => {
-      const sig = await signMessage();
-      console.log({ sig });
+  const verifySignature = useCallback(
+    async (signature) => {
+      try {
+        if (typeof account === "string") {
+          const response = await mutateVerify({ address: account, signature });
 
-      document.cookie = "jwt=0xiPledgeToApe;";
+          const verify = response.data.VerifySignature;
 
-      toast.closeAll();
-
-      toastIdRef.current = toast({
-        title: "Successful Connection",
-        duration: 3000,
-        status: "success",
-        position: "top",
-        isClosable: true,
-      });
+          return verify;
+        }
+      } catch (err) {
+        console.log({ verifySignature: err });
+        return true;
+      }
     },
-    onSuccess: ({ data }) => {
-      console.log({ data });
-    },
-  });
-
-  const { mutate } = mutation;
+    [account, mutateVerify]
+  );
 
   // check if we're connected to the server
   useEffect(() => {
@@ -116,7 +144,28 @@ function MetamaskProvider({
 
         // if user is logged in to MM and has not yet authenticated with server
         if (isAuthorized && networkActive && loaded && !networkError && !jwt) {
-          await mutate({ address: account });
+          const nonce = await getNonce();
+
+          const signer = library.getSigner();
+          const sig = await signer.signMessage(nonce);
+
+          console.log({ sig });
+
+          const verify = await verifySignature(sig);
+
+          console.log({ verify });
+
+          document.cookie = "jwt=0xiPledgeToApe;";
+
+          toast.closeAll();
+
+          toastIdRef.current = toast({
+            title: "Successful Connection",
+            duration: 3000,
+            status: "success",
+            position: "top",
+            isClosable: true,
+          });
         }
       } catch (err) {
         console.log(err);
@@ -145,7 +194,8 @@ function MetamaskProvider({
     toast,
     deactivateNetwork,
     account,
-    mutate,
+    getNonce,
+    verifySignature,
   ]);
 
   if (loaded) {
